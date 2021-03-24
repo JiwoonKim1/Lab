1eec6655b5c73c2ff6c9fd099fbf93a42fb1a55c
Jiwoon Kim <jiwoon.kim@wrl.onl>
2021-03-24 16:27:26 +0900

    Add Cylinder Rotation script

---
+++ Assets/Script/CylinderRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderRotation : MonoBehaviour
+{
+    public float degreePerSec = 0;
+
+    private float x = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += Time.deltaTime * degreePerSec;
+        transform.rotation = Quaternion.Euler(0, x, 0);
+    }
+}
