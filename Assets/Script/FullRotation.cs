75ba5c41c9e04b391a4258f23e0f7723045a2aeb
Jiwoon Kim <jiwoon.kim@wrl.onl>
2021-03-31 17:07:58 +0900

    Add two Directional Lights

--- Assets/Script/FullRotation.cs
+++ Assets/Script/FullRotation.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class FullRotation : MonoBehaviour
 {
-
     public float degreePerSec = 0;
 
-
+    private float totalRot = 0;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, degreePerSec) * Time.deltaTime);
+        if(totalRot < 360)
+        {
+            transform.Rotate(new Vector3(0, degreePerSec, 0) * Time.deltaTime);
+            totalRot += degreePerSec * Time.deltaTime;
+        }
     }
 }
