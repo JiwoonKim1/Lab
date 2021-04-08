562fc05451deafdfe18c730126342b81cfbbf0cc
Jiwoon Kim <jiwoon.kim@wrl.onl>
2021-04-08 17:57:10 +0900

    change starField scene

---
+++ Assets/Script/BalanceScene/Starts/BackgroundStars.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundStars : MonoBehaviour
+{
+    private ParticleSystem.Particle[] points;
+
+    public int starsMax = 100;
+    public float starSize = 1f;
+    public float starClipDistance = 1f;
+    public float speed = 1f;
+    public Camera camera;
+
+    private Transform tx;
+
+    private Vector3 mySpeed;
+
+    public float bigRadius = 8f;
+    public float smallRadius = 4f;
+
+    private float bigRadiusSqr;
+    private float smallRadiusSqr;
+    private float starClipDistanceSqr;
+    private float delta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        smallRadiusSqr = smallRadius * smallRadius;
+        bigRadiusSqr = bigRadius * bigRadius;
+        delta = bigRadius - smallRadius;
+        starClipDistanceSqr = starClipDistance * starClipDistance;
+
+        tx = this.transform;
+
+        mySpeed = new Vector3(0, 0, speed);
+
+    }
+
+
+    private void CreateStars()
+    {
+
+        points = new ParticleSystem.Particle[starsMax];
+
+        //카메라 위치에서 반지름 starDistance인 구 안에 별이 생성됨
+        for (int i = 0; i < starsMax; i++)
+        {
+            points[i].position = Random.onUnitSphere * bigRadius;
+            points[i].color = new Color(1, 1, 1, 1);
+            points[i].size = starSize;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("update");
+        if (points == null) CreateStars();
+
+        for (int i = 0; i < starsMax; i++)
+        {
+            points[i].position += mySpeed * Time.deltaTime;
+
+            //별이 카메라에 가까이 왔을때 알파값을 줄임, 너무 커 보이는 것을 방지하기 위해
+            if ((points[i].position - tx.transform.position).sqrMagnitude <= smallRadiusSqr)
+            {
+                float percent = (points[i].position - tx.transform.position).sqrMagnitude / smallRadiusSqr;
+
+                points[i].color = new Color(1, 1, 1, percent);
+                points[i].size = percent * starSize;
+
+            }
+            /*
+            if (((points[i].position).sqrMagnitude < smallRadiusSqr) || ((points[i].position).sqrMagnitude > bigRadiusSqr))
+            {
+                //Debug.Log("reset");
+                float length = delta * Random.value;
+                points[i].position = Random.onUnitSphere * (bigRadius - length);
+            }
+            */
+
+            //speed *  z >0 && 특정거리 이상인 경우 위치 재지정
+            if (points[i].position.z * speed > speed * (-1))
+            {
+                //Debug.Log(points[i].position);
+                points[i].position = Random.onUnitSphere * bigRadius;
+            }
+
+        }
+
+        GetComponent<ParticleSystem>().SetParticles(points, points.Length);
+
+    }
+}
--- Assets/Script/BalanceScene/Starts/InfiniteStarfield.cs
+++ Assets/Script/BalanceScene/Starts/InfiniteStarfield.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InfiniteStarfield : MonoBehaviour
 {
@@ -11,12 +9,14 @@ public class InfiniteStarfield : MonoBehaviour
     public float starSize = 1;
     public float starDistance = 10;
     public float starClipDistance = 1;
+
     private float starDistanceSqr;
     private float starClipDistanceSqr;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Debug.Log(starDistance);
         tx = this.transform;
         starDistanceSqr = starDistance * starDistance;
         starClipDistanceSqr = starClipDistance * starClipDistance;
@@ -24,14 +24,17 @@ public class InfiniteStarfield : MonoBehaviour
      
     private void CreateStars()
     {
+       // Debug.Log("CreateStars");
+
         points = new ParticleSystem.Particle[starsMax];
 
         //카메라 위치에서 반지름 starDistance인 구 안에 별이 생성됨
         for(int i =0; i < starsMax; i++)
         {
-            points[i].position = Random.insideUnitSphere * starDistance + tx.position;
+            points[i].position = tx.position + Random.insideUnitSphere * starDistance;
             points[i].color = new Color(1, 1, 1, 1);
             points[i].size = starSize;
+            //ebug.Log(points[i].position);
         }
     }
 
@@ -45,7 +48,8 @@ public class InfiniteStarfield : MonoBehaviour
             //현재 위치에서 별이 너무 멀어졌을때, 별의 위치를 재정의
             if((points[i].position - tx.position).sqrMagnitude > starDistanceSqr)
             {
-                points[i].position = Random.insideUnitSphere.normalized * starDistance + tx.position;
+                points[i].position = tx.position + Random.insideUnitSphere.normalized * starDistance;
+                //Debug.Log(tx.position);
             }
 
             //별이 카메라에 가까이 왔을때 알파값을 줄임, 너무 커 보이는 것을 방지하기 위해
@@ -54,6 +58,7 @@ public class InfiniteStarfield : MonoBehaviour
                 float percent = (points[i].position - tx.position).sqrMagnitude / starClipDistanceSqr;
                 points[i].color = new Color(1, 1, 1, percent);
                 points[i].size = percent * starSize;
+                //Debug.Log(percent);
             }
         }
 
